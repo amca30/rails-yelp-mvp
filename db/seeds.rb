5685b3a9e8c391d29e10b997038fd9b281ea8371
Amelie Caron <caron.amelie94@gmail.com>
2024-05-22 17:00:18 +0200

    exo2

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,13 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+5.times do
+  restaurant = Restaurant.new(
+    name:    Faker::Company.name,
+    phone_number: Faker::PhoneNumber.phone_number,
+    category: ["chinese", "italian", "japanese", "french", "belgian"].sample,
+    address: Faker::Address.full_address,
+  )
+  restaurant.save!
+end
